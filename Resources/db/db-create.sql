ca44b3ef4140fe83bee7650bffc767c5358449fc
eboissier <eboissier@e07a6ad9-7c28-0410-a8f0-b6e01509c4f6>
2014-07-15 16:12:22 +0000

    git-svn-id: https://gateway.terradue.com/repo/products/sugar/Terradue.News/trunk@37025 e07a6ad9-7c28-0410-a8f0-b6e01509c4f6

--- Resources/db/db-create.sql
+++ Resources/db/db-create.sql
@@ -17,9 +17,9 @@ CALL add_type($ID$, 'Terradue.News.TwitterNews, Terradue.News', 'Terradue.Portal
 -- RESULT
 
 -- Adding keys, token and pwd
-INSERT INTO config (`name`, `type`, `caption`, `hint`, `value`, `optional`) VALUES ('Tumblr-apiKey', 'string', 'Tumblr Application Api Key', 'Enter the value of the Tumblr api key', '5dmloJh2jq9ldxN9nFZdA477Kb4XrwtZQR3hLsjl0eW2HlsS0N', '0');
-INSERT INTO config (`name`, `type`, `caption`, `hint`, `value`, `optional`) VALUES ('Twitter-consumerKey', 'string', 'Twitter Application Consumer Key', 'Enter the value of the Twitter consumer key', 'zURZoLKwL1kW8l1v7ujkxDF8A', '0');
-INSERT INTO config (`name`, `type`, `caption`, `hint`, `value`, `optional`) VALUES ('Twitter-consumerSecret', 'string', 'Twitter Application Consumer Secret', 'Enter the value of the Twitter consumer secret', '6KtHzvZRQlp2JBZ8Y2fL8dzKl39APmQllHUBtKNjTDBloamTEN', '0');
-INSERT INTO config (`name`, `type`, `caption`, `hint`, `value`, `optional`) VALUES ('Twitter-token', 'string', 'Twitter Application Token', 'Enter the value of the Twitter token', '1196754241-iuj7ZgIqZwk2YpsrWC9fLnmnUH6CjA4f5M9i6hI', '0');
-INSERT INTO config (`name`, `type`, `caption`, `hint`, `value`, `optional`) VALUES ('Twitter-tokenSecret', 'string', 'Twitter Application Token Secret', 'Enter the value of the Twitter token secret', 'aB92cfpkONwXOToA04ykA1Dnd6zP2Ui67y2CbkLI9mQ3R', '0');
+INSERT INTO config (`name`, `type`, `caption`, `hint`, `optional`) VALUES ('Tumblr-apiKey', 'string', 'Tumblr Application Api Key', 'Enter the value of the Tumblr api key', '0');
+INSERT INTO config (`name`, `type`, `caption`, `hint`, `optional`) VALUES ('Twitter-consumerKey', 'string', 'Twitter Application Consumer Key', 'Enter the value of the Twitter consumer key', '0');
+INSERT INTO config (`name`, `type`, `caption`, `hint`, `optional`) VALUES ('Twitter-consumerSecret', 'string', 'Twitter Application Consumer Secret', 'Enter the value of the Twitter consumer secret', '0');
+INSERT INTO config (`name`, `type`, `caption`, `hint`, `optional`) VALUES ('Twitter-token', 'string', 'Twitter Application Token', 'Enter the value of the Twitter token', '0');
+INSERT INTO config (`name`, `type`, `caption`, `hint`, `optional`) VALUES ('Twitter-tokenSecret', 'string', 'Twitter Application Token Secret', 'Enter the value of the Twitter token secret', '0');
 -- RESULT
