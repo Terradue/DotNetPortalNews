fc05808f327604062a9e840f3fc58e3e840524b9
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2015-09-22 10:35:37 +0200

    more doc
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

--- Terradue.News/Properties/AssemblyInfo.cs
+++ Terradue.News/Properties/AssemblyInfo.cs
@@ -1,6 +1,33 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
+/*!
+
+\namespace Terradue.News
+@{
+    Terradue.News Software Package
+
+    \xrefitem sw_version "Versions" "Software Package Version" 1.0.11
+
+    \xrefitem sw_link "Link" "Software Package Link" [Terradue.New](https://git.terradue.com/sugar/terradue-news)
+
+    \xrefitem sw_license "License" "Software License" [incubating](https://git.terradue.com/sugar/terradue-news)
+
+    \xrefitem sw_req "Require" "Software Dependencies" \ref Terradue.OpenSearch
+
+    \xrefitem sw_req "Require" "Software Dependencies" \ref Terradue.OpenSearch.Tumblr
+
+    \xrefitem sw_req "Require" "Software Dependencies" \ref Terradue.OpenSearch.Twitter
+
+    \xrefitem sw_req "Require" "Software Dependencies" \ref Terradue.ServiceModel.Ogc.OwsContext
+
+    \xrefitem sw_req "Require" "Software Dependencies" \ref Terradue.Portal
+
+    \ingroup Community
+@}
+
+*/
+
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
@@ -9,7 +36,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("enguerran")]
+[assembly: AssemblyCopyright("Terradue")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
