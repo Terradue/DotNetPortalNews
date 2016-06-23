6b788fcf8d8e606a9950d1709db3fc951b93865a
eboissier <enguerran.boissier@terradue.com>
2016-06-23 15:31:54 +0200

    release

--- Terradue.News/Properties/AssemblyInfo.cs
+++ Terradue.News/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@
 @{
     Terradue .Net Library to handle multi-source news (Atom/RSS, Twitter, Tumblr...)
 
-    \xrefitem sw_version "Versions" "Software Package Version" 1.0.12
+    \xrefitem sw_version "Versions" "Software Package Version" 1.0.13
 
     \xrefitem sw_link "Link" "Software Package Link" [Terradue.News](https://git.terradue.com/sugar/terradue-news)
 
@@ -40,7 +40,7 @@ using NuGet4Mono.Extensions;
 [assembly: AssemblyLicenseUrl("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.12")]
-[assembly: AssemblyInformationalVersion("1.0.12")]
+[assembly: AssemblyVersion("1.0.13")]
+[assembly: AssemblyInformationalVersion("1.0.13")]
 
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
